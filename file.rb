65668c912551757e072a91575dd2d35b0c2735fa
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-09-10 18:06:21 +0900

    Update file.rb

--- file.rb
+++ file.rb
@@ -1,6 +1,6 @@
 require 'FileUtils'
 
-FileUtils.mkdir(installdir + fontname)
+FileUtils.mkdir_p(installdir + fontname)
 FileUtils.mv(expanddir + "/*", installdir + fontname)
 FileUtils.rm_r(fontname)
 FileUtils.rm(zipname)
