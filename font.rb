910e6d010d3f0653af49969c0340c97578677c44
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-09-11 18:22:23 +0900

    Update font.rb

--- font.rb
+++ font.rb
@@ -3,18 +3,26 @@ def fontlist
   puts "Font List"
   puts "inter"
   puts "roboto-mono"
+  puts "open-sans"
 end
 
 if font == "inter" then
-  $zipname="roboto-mono.zip"
-  $downloadfile="https://fonts.google.com/download?family=Roboto+Mono"
-  $fontname="Roboto_Mono"
-  $expanddir="Roboto_Mono/static"
+  $zipname = "roboto-mono.zip"
+  $downloadfile = "https://fonts.google.com/download?family=Roboto+Mono"
+  $fontname = "Roboto_Mono"
+  $expanddir = "Roboto_Mono/static"
 end
 
 if font == "roboto-mono" then
-  $zipname="inter.zip"
-  $downloadfile="https://fonts.google.com/download?family=Inter"
-  $fontname="Inter"
-  $expanddir="Inter/static"
+  $zipname = "inter.zip"
+  $downloadfile = "https://fonts.google.com/download?family=Inter"
+  $fontname = "Inter"
+  $expanddir = "Inter/static"
+end
+
+if font == "open-sans"
+  $zipname = "open-sans.zip"
+  $downloadfile = "https://fonts.google.com/download?family=Open+Sans"
+  $fontname = "Open_Sans"
+  $expanddir = "Open_Sans"
 end
