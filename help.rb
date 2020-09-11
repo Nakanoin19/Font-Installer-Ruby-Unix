8cefc2d3f31543a3daa99ca8498b33fbe42eb84b
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-09-11 17:32:34 +0900

    Update help.rb

--- help.rb
+++ help.rb
@@ -1,6 +1,9 @@
-def help then
-require './font'
-puts "Font Installer Help"
-puts "ruby ./install.rb [fontname]"
-fontlist
+def help
+  require './font'
+  puts "Font Installer Help"
+  puts "Execution Method"
+  puts "ruby ./install.rb [fontname]"
+  fontlist
+  puts "If you need help, please run it with the word '--help'."
+  puts "You don't need to enter the font name when you run it with the word '--help'."
 end
