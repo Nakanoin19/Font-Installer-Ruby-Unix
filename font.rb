e7338c86a758d9401d6869779aa43de6fa263f50
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-09-11 18:33:58 +0900

    Update font.rb

--- font.rb
+++ font.rb
@@ -4,6 +4,8 @@ def fontlist
   puts "inter"
   puts "roboto-mono"
   puts "open-sans"
+  puts "oswald"
+  puts "raleway"
 end
 
 if font == "inter" then
@@ -20,9 +22,28 @@ if font == "roboto-mono" then
   $expanddir = "Inter/static"
 end
 
-if font == "open-sans"
+if font == "open-sans" then
   $zipname = "open-sans.zip"
   $downloadfile = "https://fonts.google.com/download?family=Open+Sans"
   $fontname = "Open_Sans"
   $expanddir = "Open_Sans"
+end
+
+if font == "oswald" then
+  $zipname = "oswald.zip"
+  $downloadfile = "https://fonts.google.com/download?family=Oswald"
+  $fontname = "Oswald"
+  $expanddir = "Oswald"
+end
+if font == "raleway" then
+  $zipname = "raleway.zip"
+  $downloadfile = "https://fonts.google.com/download?family=Raleway"
+  $fontname = "Raleway"
+  $expanddir = "Raleway"
+end
+if font == "poppins" then
+  $zipname = "poppins.zip"
+  $downloadfile = "https://fonts.google.com/download?family=Poppins"
+  $fontname = "Poppins"
+  $expanddir = "Poppins/static"
 end
