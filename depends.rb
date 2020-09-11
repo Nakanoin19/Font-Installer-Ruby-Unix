f88e6786475ae675b900e9027ddbd4c4d21acd32
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-09-11 20:37:13 +0900

    Update depends.rb

--- depends.rb
+++ depends.rb
@@ -1,18 +1,18 @@
-print "It will install 'FileUtils'. Is that alright?[Y/n]"
+print "It'll install 'FileUtils'. Is that alright?[Y/n]"
 yn1 = gets.chomp
 case yn1 
   when /^[Yy]/
     system('gem install fileutils')
 end
 
-print "It will install 'Rubyzip'. Is that alright?[Y/n]"
+print "It'll install 'Rubyzip'. Is that alright?[Y/n]"
 yn2 = gets.chomp
 case yn2
   when /^[Yy]/
     system('gem install rubyzip')
 end
 
-print "It will install 'OpenUri'. Is that alright?[Y/n]"
+print "It'll install 'OpenUri'. Is that alright?[Y/n]"
 yn3 = gets.chomp
 case yn3
   when /^[Yy]/
