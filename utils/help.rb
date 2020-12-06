af91bdfbd95351ea7804e43718d7ce120005b9d7
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-12-06 16:14:18 +0900

    Delete help.rb

--- utils/help.rb
+++
@@ -1,9 +0,0 @@
-def help
-  require './font'
-  puts "Font Installer Help"
-  puts "Execution Method"
-  puts "ruby ./install.rb [fontname]"
-  fontlist
-  puts "If you need help, please run it with the word '--help'."
-  puts "You don't need to enter the font name when you run it with the word '--help'."
-end
