254c573ad90c9b16424b746dde3aa9a25eb2c679
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-12-06 16:11:52 +0900

    Delete file.rb

--- utils/file.rb
+++
@@ -1,8 +0,0 @@
-def file
-  require 'FileUtils'
-  
-  FileUtils.mkdir_p(installdir + fontname)
-  FileUtils.mv(expanddir + allfile, installdir + fontname)
-  FileUtils.rm_r(fontname)
-  FileUtils.rm(zipname)
-end
